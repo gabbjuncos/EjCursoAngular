93d31ed123fb3542099ae9713aada11871b97cb5
unknown <gabbjuncos@gmail.com>
2022-04-28 11:28:39 -0300

    3.3 eje 1

---
+++ 3. Base de TypeScript - Sección recomendada/ts-intro/src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,28 @@
+
+
+//console.log('Hola Mundo!');
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre:string = 'Strider';
+// con const no dice porque nunca va a cambiar el valor
+
+const a = "hola";
+
+nombre = "Puntos de vida: ";
+console.log(nombre);
+
+// puedo decir los tipos de datos que puede tomar la variable 
+// puedo tomar valores tipo numero o string 
+let hp: number|string = 95;
+
+hp = 'FULL';
+
+console.log(nombre,hp);
+
+let estaVivo: boolean = true;
+console.log("Esta vido: ",estaVivo);
+
+
