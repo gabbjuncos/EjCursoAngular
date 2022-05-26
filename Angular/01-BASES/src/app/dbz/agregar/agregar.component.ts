77ea5a7b87e09035e33ee5d054167217ef80601c
unknown <gabbjuncos@gmail.com>
2022-05-26 14:01:43 -0300

    LISTO HASTA 5.13

--- Angular/01-BASES/src/app/dbz/agregar/agregar.component.ts
+++ Angular/01-BASES/src/app/dbz/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input} from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
+
 import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
@@ -8,19 +9,23 @@ import { Personaje } from '../interfaces/dbz.interface';
 })
 export class AgregarComponent {
   // recibir los personajes como inputs
-  @Input() personajes: Personaje[] = [];
+  //@Input() personajes: Personaje[] = [];
 
-  @Input() nuevo: Personaje ={
+  @Input() nuevo: Personaje = {
       // para que tenga datos por defecto
     nombre:'Trucks',
     poder:14000
   }
 
+  @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter<Personaje>();
+  
   agregar(){
-    if(this.nuevo.nombre.trim().length == 0){return;}
-    console.log(this.nuevo)
+    if(this.nuevo.nombre.trim().length === 0){return;}
+    console.log(this.nuevo);
+    this.onNuevoPersonaje.emit(this.nuevo);
+
     // agregar al arreglo
-    this.personajes.push(this.nuevo)
+    //this.personajes.push(this.nuevo)
     // inicializamos de nuevo al obj pesonaje nuevo
     this.nuevo = {
       nombre:'',
